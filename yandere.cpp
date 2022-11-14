fb26c5be267339b9024d6669df09b5c57d15b9ad
Star Rauchenberger <fefferburbia@gmail.com>
2022-11-13 22:29:05 -0500

    Bot now uses Mastodon instead of Twitter

--- yandere.cpp
+++ yandere.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <sstream>
 #include <fstream>
-#include <twitter.h>
+#include <mastodonpp.hpp>
 #include <chrono>
 #include <thread>
 #include <algorithm>
@@ -12,17 +12,17 @@ template <class InputIterator>
 std::string implode(InputIterator first, InputIterator last, std::string delimiter)
 {
   std::stringstream result;
-  
+
   for (InputIterator it = first; it != last; it++)
   {
     if (it != first)
     {
       result << delimiter;
     }
-    
+
     result << *it;
   }
-  
+
   return result.str();
 }
 
@@ -30,52 +30,41 @@ template <class Container>
 Container split(std::string input, std::string delimiter)
 {
   Container result;
-  
+
   while (!input.empty())
   {
     int divider = input.find(delimiter);
     if (divider == std::string::npos)
     {
       result.push_back(input);
-      
+
       input = "";
     } else {
       result.push_back(input.substr(0, divider));
-      
+
       input = input.substr(divider+delimiter.length());
     }
   }
-  
+
   return result;
 }
 
-int main(int argc, char** argv)
+void run(const std::string& configfile)
 {
-  if (argc != 2)
-  {
-    std::cout << "usage: yandere [configfile]" << std::endl;
-    return -1;
-  }
-
-  std::string configfile(argv[1]);
   YAML::Node config = YAML::LoadFile(configfile);
-  
-  twitter::auth auth;
-  auth.setConsumerKey(config["consumer_key"].as<std::string>());
-  auth.setConsumerSecret(config["consumer_secret"].as<std::string>());
-  auth.setAccessKey(config["access_key"].as<std::string>());
-  auth.setAccessSecret(config["access_secret"].as<std::string>());
-  
-  twitter::client client(auth);
-  
+
+  mastodonpp::Instance instance{
+    config["mastodon_instance"].as<std::string>(),
+    config["mastodon_token"].as<std::string>()};
+  mastodonpp::Connection connection{instance};
+
   std::map<std::string, std::vector<std::string>> groups;
   std::ifstream datafile(config["forms"].as<std::string>());
   if (!datafile.is_open())
   {
     std::cout << "Could not find forms file." << std::endl;
-    return 1;
   }
-  
+
   bool newgroup = true;
   std::string line;
   std::string curgroup;
@@ -85,7 +74,7 @@ int main(int argc, char** argv)
     {
       line.pop_back();
     }
-    
+
     if (newgroup)
     {
       curgroup = line;
@@ -112,10 +101,10 @@ int main(int argc, char** argv)
 
   for (;;)
   {
-    std::cout << "Generating tweet" << std::endl;
-    
+    std::cout << "Generating post" << std::endl;
+
     std::map<std::string, std::string> variables;
-    
+
     std::string action = "{MAIN}";
     int tknloc;
     while ((tknloc = action.find("{")) != std::string::npos)
@@ -128,7 +117,7 @@ int main(int argc, char** argv)
         modifier = token.substr(modloc+1);
         token = token.substr(0, modloc);
       }
-      
+
       int eqloc;
       std::string eqvarname;
       if ((eqloc = token.find("=")) != std::string::npos)
@@ -136,12 +125,12 @@ int main(int argc, char** argv)
         eqvarname = token.substr(0, eqloc);
         token = token.substr(eqloc+1);
       }
-      
+
       std::string canontkn;
       std::transform(std::begin(token), std::end(token), std::back_inserter(canontkn), [] (char ch) {
         return std::toupper(ch);
       });
-      
+
       std::string result;
       if (canontkn == "\\N")
       {
@@ -155,12 +144,12 @@ int main(int argc, char** argv)
 
         result = group[dist(random_engine)];
       }
-      
+
       if (!eqvarname.empty())
       {
         variables[eqvarname] = result;
       }
-      
+
       if (modifier == "indefinite")
       {
         if ((result.length() > 1) && (isupper(result[0])) && (isupper(result[1])))
@@ -173,7 +162,7 @@ int main(int argc, char** argv)
           result = "a " + result;
         }
       }
-      
+
       std::string finalresult;
       if (islower(token[0]))
       {
@@ -187,30 +176,56 @@ int main(int argc, char** argv)
         {
           word[0] = std::toupper(word[0]);
         }
-        
+
         finalresult = implode(std::begin(words), std::end(words), " ");
       } else {
         finalresult = result;
       }
-      
+
       action.replace(tknloc, action.find("}")-tknloc+1, finalresult);
     }
-    
-    action.resize(140);
-    
-    try
+
+    const mastodonpp::parametermap parameters{{"status", action}};
+    auto answer{connection.post(mastodonpp::API::v1::statuses, parameters)};
+    if (!answer)
     {
-      client.updateStatus(action);
-    } catch (const twitter::twitter_error& e)
-    {
-      std::cout << "Twitter error: " << e.what() << std::endl;
+      if (answer.curl_error_code == 0)
+      {
+        std::cout << "HTTP status: " << answer.http_status << std::endl;
+      }
+      else
+      {
+        std::cout << "libcurl error " << std::to_string(answer.curl_error_code)
+             << ": " << answer.error_message << std::endl;
+      }
     }
-    
+
     std::cout << action << std::endl;
     std::cout << "Waiting" << std::endl;
-    
+
     std::this_thread::sleep_for(std::chrono::hours(1));
-    
+
     std::cout << std::endl;
   }
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    std::cout << "usage: yandere [configfile]" << std::endl;
+    return -1;
+  }
+
+  std::string configfile(argv[1]);
+
+  try
+  {
+    run(configfile);
+  } catch (const mastodonpp::CURLException& e)
+  {
+    std::cout << e.what() << std::endl;
+  }
+
+  return 0;
 }
